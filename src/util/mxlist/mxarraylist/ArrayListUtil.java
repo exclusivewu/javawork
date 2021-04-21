1ae4f9a9fdcdee1f796ab1878337dd55ebe0c559
武鑫伟 <1029137541@qq.com>
2021-04-21 22:18:37 +0800

    增加目录

---
+++ src/util/mxlist/mxarraylist/ArrayListUtil.java
@@ -0,0 +1,9 @@
+package util.mxlist.mxarraylist;
+
+/**
+ * @Author: Wuxinwei
+ * @Date: 2021/4/21 22:18
+ * @Description:
+ */
+public class ArrayListUtil {
+}
---
+++ src/util/mxlist/mxlinkedlist/LinkelistUtil.java
@@ -0,0 +1,9 @@
+package util.mxlist.mxlinkedlist;
+
+/**
+ * @Author: Wuxinwei
+ * @Date: 2021/4/21 22:17
+ * @Description:
+ */
+public class LinkelistUtil {
+}
