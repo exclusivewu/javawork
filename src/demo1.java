ca1b5c3de1753c20cef55abfd38a73b2a14f8fd0
武鑫伟 <1029137541@qq.com>
2021-04-21 22:07:22 +0800

    新增代码

---
+++ src/demo1.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Wuxinwei
+ * @Date: 2021/4/21 21:59
+ * @Description: 测试代码
+ */
+public class demo1 {
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+
+}
